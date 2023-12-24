3a24803b8ceb45f7289b5f3834578d8f9a266d88
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-12-24 18:22:44 -0300

    Created method to registry a subscription and send an email.

--- src/main/java/microsservice/eventsservice/services/EventService.java
+++ src/main/java/microsservice/eventsservice/services/EventService.java
@@ -35,4 +35,24 @@ public class EventService {
         Event event = new Event(eventRequestDTO);
         return eventRepository.save(event);
     }
+
+    public Boolean isEventFull(Event event){
+        Long registeredParticipantsQuantity = subscriptionRepository.getSubscriptionsQuantityByEvent(event.getId());
+        return event.getMaxParticipants() >= registeredParticipantsQuantity;
+    }
+
+    public void registerParticipant(String eventId, String participantEmail){
+        Event event = eventRepository.findById(eventId).orElseThrow(EventNotFoundException::new);
+        if(isEventFull(event)){
+            throw new EventFullException();
+        }
+
+        Subscription subscription = new Subscription(event, participantEmail);
+        subscriptionRepository.save(subscription);
+
+        EmailRequestDTO emailRequestDTO = new EmailRequestDTO(participantEmail, "Event Registration.",
+                "You have been successfully registered for the event!");
+
+        emailServiceClient.sendEmail(emailRequestDTO);
+    }
 }
