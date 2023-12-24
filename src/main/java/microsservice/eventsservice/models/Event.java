f3aa3a016ae3fbfa444472d4e18ff07ca36e7989
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-12-24 16:53:07 -0300

    Created Event constructor with EventRequestDTO parameter.

--- src/main/java/microsservice/eventsservice/models/Event.java
+++ src/main/java/microsservice/eventsservice/models/Event.java
@@ -3,6 +3,7 @@ package microsservice.eventsservice.models;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import microsservice.eventsservice.dtos.EventRequestDTO;
 
 @Entity(name="event")
 @Table(name="event")
@@ -20,4 +21,11 @@ public class Event {
     private String title;
 
     private String description;
+
+    public Event(EventRequestDTO eventDTO){
+        this.maxParticipants = eventDTO.maxParticipants();
+        this.date = eventDTO.date();
+        this.title = eventDTO.title();
+        this.description = eventDTO.description();
+    }
 }
