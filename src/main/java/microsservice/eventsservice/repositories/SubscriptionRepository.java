4e0fedf88dfbcef3829d48f39c3f43b92a589ec2
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-12-24 17:48:31 -0300

    Created getSubscriptionsQuantityByEvent method

--- src/main/java/microsservice/eventsservice/repositories/SubscriptionRepository.java
+++ src/main/java/microsservice/eventsservice/repositories/SubscriptionRepository.java
@@ -2,6 +2,10 @@ package microsservice.eventsservice.repositories;
 
 import microsservice.eventsservice.models.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+
+    @Query("SELECT COUNT(s) FROM subscription s WHERE s.event.id = :eventId")
+    Long getSubscriptionsQuantityByEvent(@Param("eventId") String eventId);
 }
