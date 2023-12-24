c1ce42bd1c857ba31876d2e090fdf02a683c894f
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-12-24 14:51:31 -0300

    Created Event model

---
+++ src/main/java/model/Event.java
@@ -0,0 +1,23 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity(name="event")
+@Table(name="event")
+@Data
+@EqualsAndHashCode(of="id")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private int maxParticipants;
+
+    private String date;
+
+    private String title;
+
+    private String description;
+}
