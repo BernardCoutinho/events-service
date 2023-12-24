f2c1e17540ae1e27e98aefc7fb8ca80e123e4a16
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-12-24 18:40:38 -0300

    Created EventController

---
+++ src/main/java/microsservice/eventsservice/controllers/EventController.java
@@ -0,0 +1,17 @@
+package microsservice.eventsservice.controllers;
+
+import microsservice.eventsservice.dtos.EventRequestDTO;
+import microsservice.eventsservice.dtos.SubscriptionRequestDTO;
+import microsservice.eventsservice.models.Event;
+import microsservice.eventsservice.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/event")
+public class EventController {
+
+
+}
