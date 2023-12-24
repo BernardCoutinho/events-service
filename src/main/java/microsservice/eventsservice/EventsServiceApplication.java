3266405442addcf448c5b217ff23870cc03f87bf
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-12-24 16:51:37 -0300

    EnableFeignClients configuration

--- src/main/java/microsservice/eventsservice/EventsServiceApplication.java
+++ src/main/java/microsservice/eventsservice/EventsServiceApplication.java
@@ -2,8 +2,10 @@ package microsservice.eventsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EventsServiceApplication {
 
 	public static void main(String[] args) {
