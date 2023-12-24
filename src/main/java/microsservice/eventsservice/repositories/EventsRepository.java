3a42f35d51b22c79ec33ee81f31a19e7bafed75e
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-12-24 15:22:01 -0300

    Created query to search for upcoming events and override native JPA findById

--- src/main/java/microsservice/eventsservice/repositories/EventsRepository.java
+++ src/main/java/microsservice/eventsservice/repositories/EventsRepository.java
@@ -2,6 +2,19 @@ package microsservice.eventsservice.repositories;
 
 import microsservice.eventsservice.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import jakarta.annotation.Nonnull;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface EventsRepository extends JpaRepository<Event, String> {
+
+    @Query(value = "SELECT * FROM events event WHERE PARSE (event.date AS TIMESTAMP > :currentDate)", nativeQuery = true)
+    List<Event> findUpcomingEvents(@Param("currentDate") LocalDateTime currentDate);
+
+    @Nonnull
+    Optional<Event> findById(@Nonnull String id);
 }
