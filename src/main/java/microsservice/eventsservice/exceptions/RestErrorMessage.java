34dea38ed2c0887762f44507d932fa501e1ddda6
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-12-25 12:14:32 -0300

    Created RestErrorMessage as exception class pattern

---
+++ src/main/java/microsservice/eventsservice/exceptions/RestErrorMessage.java
@@ -0,0 +1,16 @@
+package microsservice.eventsservice.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class RestErrorMessage {
+    private HttpStatus status;
+
+    private String errorMessage;
+}
